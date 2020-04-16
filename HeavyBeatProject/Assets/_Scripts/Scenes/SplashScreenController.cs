72886ec416efffd0ac5ffdf5b3f47e27d2918907
Doppeltje <Doppeltje@users.noreply.github.com>
2020-04-16 23:55:01 +0200

    added main menu.

---
+++ HeavyBeatProject/Assets/_Scripts/Scenes/SplashScreenController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SplashScreenController : MonoBehaviour {
+    private AudioManager _audioManager;
+    
+    IEnumerator Start()
+    {
+        SplashScreen.Begin();
+        _audioManager = FindObjectOfType<AudioManager>();
+        _audioManager.Play("");
+        while (!SplashScreen.isFinished) {
+            SplashScreen.Draw();
+            yield return null;
+        }
+        Debug.Log("Finished showing splash screen");
+    }
+}
