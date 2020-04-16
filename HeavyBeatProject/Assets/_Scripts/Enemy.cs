c245b5d4233b5c4ed8e3f8ab5ba484b5bceed407
Doppeltje <Doppeltje@users.noreply.github.com>
2020-04-16 15:22:15 +0200

    Added enemy script. commiting before pulling development

---
+++ HeavyBeatProject/Assets/_Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField] private int _amountOfPlayers = 2;
+
+    [SerializeField] private GameObject[] _players;
+    private Dictionary<GameObject, bool> _playerHitMap;
+
+
+    private void Start() {
+        InitializePlayerHitMap();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.X)) {
+            SetPlayerHit(0);
+        }
+    }
+
+    private void InitializePlayerHitMap() {
+        _playerHitMap = new Dictionary<GameObject, bool>();
+        
+        for (int i = 0; i < _amountOfPlayers; i++) {
+            _playerHitMap[_players[i]] = false;
+            print("Player " + i + "value: " + _playerHitMap[_players[i]]);
+        }
+    }
+
+    private void SetPlayerHit(int id) {
+        _playerHitMap[_players[id]] = true;
+        print("Player " + _players[id].gameObject.name + " has hit " + gameObject);
+        CheckIfDestroy();
+    }
+
+    private void CheckIfDestroy() {
+        bool check = false;
+        
+        for (int i = 0; i < _amountOfPlayers; i++) {
+            if (_playerHitMap[_players[i]]) {
+                check = true;
+            }
+            else {
+                check = false;
+            }
+        }
+
+        if (check) {
+            Destroy(gameObject);
+            return;
+        }
+
+        print("Not yet destroyed");
+    }
+}
