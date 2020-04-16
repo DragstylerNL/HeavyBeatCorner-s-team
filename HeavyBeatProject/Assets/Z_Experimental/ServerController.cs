b84e181e2e307bb26c409ae8c52cd93d5c5f7225
DragstylerNL <dragstylernl@gmail.com>
2020-04-16 17:11:42 +0200

    werk niet

---
+++ HeavyBeatProject/Assets/Z_Experimental/ServerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ServerController : MonoBehaviour
+{
+    private Server _server;
+    private Client _client;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _server = GetComponent<Server>();
+        _client = GetComponent<Client>();
+    }
+
+    public void StartServer()
+    {
+        _server.StartServer();
+    }
+
+    public void StartClient()
+    {
+        _client.StartClient();
+    }
+
+    public void SendMessageToServer()
+    {
+        _client.SendMessageToServer("DAAAAHLINGG");
+    }
+}
